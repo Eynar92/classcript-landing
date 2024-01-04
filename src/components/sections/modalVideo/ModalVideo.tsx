147fd0972e536bc48f27cdfb070b491d52878353
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2024-01-03 21:50:31 -0400

    Update

---
+++ src/components/sections/modalVideo/ModalVideo.tsx
@@ -0,0 +1,17 @@
+import { Particles } from "./components/Particles";
+import Modal from "./components/Modal";
+
+export default function ModalVideo() {
+    return (
+        <section className="w-screen h-96 inset-0 flex items-center relative justify-center my-20 snap-center bg-[#272e3f] lg:my-20 ">
+            <Particles className="absolute w-full h-auto inset-0" />
+            <div
+                className="flex justify-center items-center cursor-pointer"
+            >
+                <div className="h-20 w-20 bg-[#5ae0ab] rounded-full flex justify-center items-center animation-pulse">
+                    <Modal />
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/sections/modalVideo/components/Modal.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+export default function Modal() {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <svg width="3em" height="3em" viewBox="0 0 16 16" className="bi bi-play-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+                </svg>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-3xl h-auto sm:max-h-full">
+                <DialogHeader>
+                    <DialogTitle>How it Works <span className="font-extrabold">Classcript</span></DialogTitle>
+                </DialogHeader>
+                <div className="flex items-center space-x-2">
+                    <div className="flex items-center justify-center w-full h-96 border border-slate-500 rounded-lg">
+                        <span>Aquí se verá el video de Classcript</span>
+                    </div>
+                </div>
+                <DialogFooter className="sm:justify-start">
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Close
+                        </Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
